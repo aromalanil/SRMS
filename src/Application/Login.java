72f134d3d75567868505a9e645650b7ce99ecd8e
Aromal Anil <aromalanilkannan@gmail.com>
2019-11-08 19:23:02 +0530

    minor edits

--- src/Application/Login.java
+++ src/Application/Login.java
@@ -1,7 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2019 Aromal.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package Application;
 
@@ -9,7 +27,7 @@ import java.sql.*;
 
 /**
  *
- * @author cec-t100
+ * @author Aromal Anil
  */
 public class Login extends javax.swing.JFrame {
 
@@ -245,47 +263,13 @@ public class Login extends javax.swing.JFrame {
         {
             System.out.println("Connection error");
         }
-        
-//        if("teacher".equals(username.getText()))
-//        {
-//            if("teacher".equals(password.getText()))
-//            {
-//                this.setVisible(false);
-//                Teacher t = new Teacher();
-//            } else {
-//            }
-//        }
-//        if("student".equals(username.getText()))
-//        {
-//            if("student".equals(password.getText()))
-//            {
-//                this.setVisible(false);
-//                Student s = new Student();
-//            } else {
-//            }
-//        }
-//        if("head".equals(username.getText()))
-//        {
-//            if("head".equals(password.getText()))
-//            {
-//                this.setVisible(false);
-//                Hod h = new Hod();
-//            } else {
-//            }
-//        }
-//        else
-//        {
-//            jLabel8.setVisible(true);
-//        }
     }//GEN-LAST:event_jButton1MouseClicked
 
     private void passwordTextBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordTextBoxActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_passwordTextBoxActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
